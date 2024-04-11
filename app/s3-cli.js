87336ea9b6fc2dbd83e8f580fcea6fc1345df7fc
demchuck1 <dawiddemczak@gmail.com>
2024-04-11 11:23:01 +0200

    Próba dostepu do bucket'a

--- app/s3-cli.js
+++ app/s3-cli.js
@@ -1,9 +1,34 @@
-var AWS = require("@aws-sdk");
+const { S3Client, ListObjectsCommand } = require("@aws-sdk/client-s3");
 
-AWS.config.getCredentials(function (err) {
-  if (err) console.log(err.stack);
-  // credentials not loaded
-  else {
-    console.log("Access key:", AWS.config.credentials.accessKeyId);
-  }
+// Konfiguracja poświadczeń dostępu
+const credentials = {
+  accessKeyId: "AKIAVKNBWT6MRTC5NMY2",
+  secretAccessKey: "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",
+};
+
+// Tworzenie klienta S3
+const s3Client = new S3Client({
+  region: "us-east-1",
+  endpoint: "x-wing",
+  credentials,
 });
+
+// Funkcja do listowania wszystkich plików w kubełku S3
+async function listFiles() {
+  const params = {
+    Bucket: "developer-task",
+  };
+
+  try {
+    const data = await s3Client.send(new ListObjectsCommand(params));
+    console.log(
+      "Pliki w kubełku:",
+      data.Contents.map((obj) => obj.Key)
+    );
+  } catch (err) {
+    console.error("Błąd podczas listowania plików:", err);
+  }
+}
+
+// Wywołanie funkcji
+listFiles();
