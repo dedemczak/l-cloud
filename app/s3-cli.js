7d31597f23a603be266bf504e1b650bbea492231
demchuck1 <dawiddemczak@gmail.com>
2024-04-11 12:17:01 +0200

    Próba dostepu do bucketa - przez zmienne srodowiskowe enb

--- app/s3-cli.js
+++ app/s3-cli.js
@@ -3,14 +3,23 @@ const {
   SharedIniFileCredentials,
   ListObjectsCommand,
 } = require("@aws-sdk/client-s3");
+require("dotenv").config();
 
-const { fromIni } = require("@aws-sdk/credential-providers");
+// const { fromIni } = require("@aws-sdk/credential-providers");
 
-const credentials = fromIni();
+// const credentials = fromIni();
+
+// const s3Client = new S3Client({
+//   region: "eu-west-1",
+//   credentials: credentials,
+// });
 
 const s3Client = new S3Client({
   region: "eu-west-1",
-  credentials: credentials,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
 s3Client
