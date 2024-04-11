a9c2925d7d6f8565c9482268578919557b386b1f
demchuck1 <dawiddemczak@gmail.com>
2024-04-11 18:04:15 +0200

    Listowanie plików z parametrami

--- app/s3-cli.js
+++ app/s3-cli.js
@@ -1,7 +1,8 @@
 const {
   S3Client,
-  ListObjectsCommand,
   PutObjectCommand,
+  ListObjectsCommand,
+  DeleteObjectCommand,
 } = require("@aws-sdk/client-s3");
 const fs = require("fs");
 
@@ -18,37 +19,116 @@ const s3Client = new S3Client({
 const Bucket = "developer-task";
 const Prefix = "x-wing/";
 
-const listFiles = async () => {
-  const params = {
-    Bucket,
-    Prefix,
-  };
-
+const listFilesParams = {
+  Bucket,
+  Prefix,
+};
+const listFiles = async (...options) => {
   try {
-    const data = await s3Client.send(new ListObjectsCommand(params));
+    let regexPattern = null;
+    let showModifiedDate = false;
+    let showSize = false;
+
+    for (let i = 0; i < options.length; i++) {
+      const option = options[i];
+      switch (option) {
+        case "-rgx":
+          if (options[i + 1] && options[i + 1].startsWith("'")) {
+            regexPattern = options[i + 1].slice(1, -1);
+            i++; // przesuwamy index o 1 aby przejść do nastepnych argumentów
+          } else {
+            console.error("Nieprawidłowy format regexa.");
+            return;
+          }
+          break;
+        case "-m":
+          showModifiedDate = true;
+          break;
+        case "-s":
+          showSize = true;
+          break;
+        default:
+          break;
+      }
+    }
+
+    const data = await s3Client.send(new ListObjectsCommand(listFilesParams));
     data.Contents.forEach((obj) => {
-      console.log(obj.Key);
+      let fileName = obj.Key;
+      let fileParts = fileName.split("/"); // tworzymy tablcię, gdy lista zawsze zawier "prefix"
+
+      let regexMatch = true;
+      if (regexPattern) {
+        regexMatch = fileParts.some((part) =>
+          new RegExp(regexPattern).test(part)
+        );
+      }
+
+      if (regexMatch) {
+        // decydujemy czy wyświetlamy jakieś iformacje
+        console.log("Nazwa pliku:", fileName);
+
+        if (showModifiedDate) {
+          console.log("Data modyfikacji:", obj.LastModified.toISOString());
+        }
+        if (showSize) {
+          console.log("Rozmiar pliku:", obj.Size);
+        }
+      }
     });
   } catch (err) {
-    console.error("Błąd podczas listowania plików:", err);
+    console.error("Błąd", err);
   }
 };
 
-listFiles();
+listFiles("-n");
 
-const main = async () => {
-  const command = new PutObjectCommand({
+const deleteFilesByRegex = async (regexPattern) => {
+  try {
+    const data = await s3Client.send(new ListObjectsCommand(listFilesParams));
+    data.Contents.forEach(async (obj) => {
+      let fileName = obj.Key;
+      console.log(fileName);
+      let fileParts = fileName.split("/"); // znowu dzielimy na tablicę
+
+      regexMatch = fileParts.some((part) =>
+        new RegExp(regexPattern).test(part)
+      );
+
+      if (regexMatch) {
+        const deleteParams = {
+          Bucket,
+          Prefix,
+          Key: fileName,
+        };
+        try {
+          const response = await s3Client.send(
+            new DeleteObjectCommand(deleteParams)
+          );
+          console.log(`Usunięto plik: ${fileName}`);
+        } catch (deleteErr) {
+          console.error(`Błąd podczas usuwania pliku ${fileName}:`, deleteErr);
+        }
+      }
+    });
+  } catch (err) {
+    console.error("Błąd", err);
+  }
+};
+
+deleteFilesByRegex("^h.*\\.txt$");
+
+const uploadFile = async (fileName, Body) => {
+  const cmd = new PutObjectCommand({
     Bucket,
-    Key: Prefix + "hello-s3.txt",
-    Body: "Hello S3!",
+    Key: Prefix + fileName,
+    Body,
   });
 
   try {
-    const response = await s3Client.send(command);
+    const response = await s3Client.send(cmd);
     console.log(response);
   } catch (err) {
     console.error(err);
   }
 };
-
-main();
