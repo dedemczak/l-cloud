03d2401901bcd94cfa684dd7fe9bb506dc6eb67f
demchuck1 <dawiddemczak@gmail.com>
2024-04-11 16:01:22 +0200

    Próba dostepu do bucketa - brakowalo prefix x-wing

--- app/s3-cli.js
+++ app/s3-cli.js
@@ -1,18 +1,5 @@
-const {
-  S3Client,
-  SharedIniFileCredentials,
-  ListObjectsCommand,
-} = require("@aws-sdk/client-s3");
 require("dotenv").config();
-
-// const { fromIni } = require("@aws-sdk/credential-providers");
-
-// const credentials = fromIni();
-
-// const s3Client = new S3Client({
-//   region: "eu-west-1",
-//   credentials: credentials,
-// });
+const { S3Client, ListObjectsCommand } = require("@aws-sdk/client-s3");
 
 const s3Client = new S3Client({
   region: "eu-west-1",
@@ -22,12 +9,23 @@ const s3Client = new S3Client({
   },
 });
 
-s3Client
-  .send(new ListObjectsCommand({ Bucket: "developer-task" }))
-  .then((data) => {
-    console.log("sukces");
-    console.log("dane:", data);
-  })
-  .catch((err) => {
-    console.error("bład", err);
-  });
+const bucketName = "developer-task";
+const objectPrefix = "x-wing/";
+
+const listFiles = async () => {
+  const params = {
+    Bucket: bucketName,
+    Prefix: objectPrefix,
+  };
+
+  try {
+    const data = await s3Client.send(new ListObjectsCommand(params));
+    data.Contents.forEach((obj) => {
+      console.log(obj.Key);
+    });
+  } catch (err) {
+    console.error("Błąd podczas listowania plików:", err);
+  }
+};
+
+listFiles();
