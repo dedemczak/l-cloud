a9c4b8456e5f539d0fa7d3882608f6bf3e67ead2
demchuck1 <dawiddemczak@gmail.com>
2024-04-11 19:59:08 +0200

    Naprawa naprametrów z cli

--- app/s3-cli.js
+++ app/s3-cli.js
@@ -1,18 +1,23 @@
+const { Command } = require("commander");
 const {
   S3Client,
-  PutObjectCommand,
   ListObjectsCommand,
+  PutObjectCommand,
   DeleteObjectCommand,
 } = require("@aws-sdk/client-s3");
-const fs = require("fs");
 
-require("dotenv").config();
+const path = require("path");
+const dotenv = require("dotenv");
+const envPath = path.resolve(__dirname, "..", ".env");
+dotenv.config({ path: envPath });
+
+const program = new Command();
 
 const s3Client = new S3Client({
   region: "eu-west-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || undefined,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || undefined,
   },
 });
 
@@ -23,65 +28,24 @@ const listFilesParams = {
   Bucket,
   Prefix,
 };
-const listFiles = async (...options) => {
+
+const uploadFile = async (filePath) => {
   try {
-    let regexPattern = null;
-    let showModifiedDate = false;
-    let showSize = false;
+    const fileContent = fs.readFileSync(filePath);
+    const fileName = path.basename(filePath);
 
-    for (let i = 0; i < options.length; i++) {
-      const option = options[i];
-      switch (option) {
-        case "-rgx":
-          if (options[i + 1] && options[i + 1].startsWith("'")) {
-            regexPattern = options[i + 1].slice(1, -1);
-            i++; // przesuwamy index o 1 aby przejść do nastepnych argumentów
-          } else {
-            console.error("Nieprawidłowy format regexa.");
-            return;
-          }
-          break;
-        case "-m":
-          showModifiedDate = true;
-          break;
-        case "-s":
-          showSize = true;
-          break;
-        default:
-          break;
-      }
-    }
+    const params = {
+      Bucket,
+      Key: `${Prefix}${fileName}`,
+      Body: fileContent,
+    };
 
-    const data = await s3Client.send(new ListObjectsCommand(listFilesParams));
-    data.Contents.forEach((obj) => {
-      let fileName = obj.Key;
-      let fileParts = fileName.split("/"); // tworzymy tablcię, gdy lista zawsze zawier "prefix"
-
-      let regexMatch = true;
-      if (regexPattern) {
-        regexMatch = fileParts.some((part) =>
-          new RegExp(regexPattern).test(part)
-        );
-      }
-
-      if (regexMatch) {
-        // decydujemy czy wyświetlamy jakieś iformacje
-        console.log("Nazwa pliku:", fileName);
-
-        if (showModifiedDate) {
-          console.log("Data modyfikacji:", obj.LastModified.toISOString());
-        }
-        if (showSize) {
-          console.log("Rozmiar pliku:", obj.Size);
-        }
-      }
-    });
+    const data = await s3Client.send(new PutObjectCommand(params));
+    console.log("Plik został przesłany do Bucketu.", data);
   } catch (err) {
-    console.error("Błąd", err);
+    console.error("Błąd podczas przesyłania pliku do Bucketu:", err);
   }
 };
-
-listFiles("-n");
 
 const deleteFilesByRegex = async (regexPattern) => {
   try {
@@ -116,19 +80,72 @@ const deleteFilesByRegex = async (regexPattern) => {
   }
 };
 
-deleteFilesByRegex("^h.*\\.txt$");
+const listFiles = async (options) => {
+  let regexPattern = null;
+  let showModifiedDate = false;
+  let showSize = false;
 
-const uploadFile = async (fileName, Body) => {
-  const cmd = new PutObjectCommand({
-    Bucket,
-    Key: Prefix + fileName,
-    Body,
+  options.forEach((option, index) => {
+    switch (option) {
+      case "-rgx":
+        if (options[index + 1] && options[index + 1].startsWith("'")) {
+          regexPattern = options[index + 1].slice(1, -1);
+        } else {
+          console.error("Nieprawidłowy format regexa.");
+        }
+        break;
+      case "-m":
+        showModifiedDate = true;
+        break;
+      case "-s":
+        showSize = true;
+        break;
+      default:
+        break;
+    }
   });
 
   try {
-    const response = await s3Client.send(cmd);
-    console.log(response);
+    const data = await s3Client.send(new ListObjectsCommand(listFilesParams));
+    data.Contents.forEach((obj) => {
+      let fileName = obj.Key;
+      let fileParts = fileName.split("/"); // tworzymy tablcię, gdy lista zawsze zawier "prefix"
+
+      let regexMatch = true;
+      if (regexPattern) {
+        regexMatch = fileParts.some((part) =>
+          new RegExp(regexPattern).test(part)
+        );
+      }
+
+      if (regexMatch) {
+        // decydujemy czy wyświetlamy jakieś iformacje
+        console.log("Nazwa pliku:", fileName);
+
+        if (showModifiedDate) {
+          console.log("Data modyfikacji:", obj.LastModified.toISOString());
+        }
+        if (showSize) {
+          console.log("Rozmiar pliku:", obj.Size);
+        }
+      }
+    });
   } catch (err) {
-    console.error(err);
+    console.error("Błąd", err);
   }
 };
+
+// listFiles("-m");
+
+program
+  .command("list [options...]")
+  .description("Wyświetla listę plików w buckecie")
+  .option("-rgx <regex>", "Filtruje pliki za pomocą wyrażenia regularnego")
+  .option("-m", "Wyświetla datę modyfikacji plików")
+  .option("-s", "Wyświetla rozmiar plików")
+  .action((command) => {
+    const options = program.args.slice(1);
+    listFiles(options);
+  });
+
+program.parse(process.argv);
